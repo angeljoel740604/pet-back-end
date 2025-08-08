de076d1b5e4367cc1ca4f20d3f57a61fe806d0ad
angeljoel740604 <angeljoel740604@gmail.com>
2025-08-08 19:22:44 +0200

    refactoring

--- src/created-shipment/created-shipment.service.js
+++ src/created-shipment/created-shipment.service.js
@@ -11,6 +11,27 @@ module.exports = (apiCredentials, hyperion) => {
   const { getShipmentByGuid } = helper(hyperion).shipment;
   const { getHouses } = shipmentMgyClient();
 
+  const getOrCreateEntity = async (entityData, entityName) => {
+    if (!entityData) {
+      return null;
+    }
+
+    let entity = await getCustomerByName(entityName);
+    if (!entity) {
+      try {
+        const response = await soapApiEntity(apiCredentials).createEntity(
+          entityData
+        );
+        if (response && response.status === "Imported") {
+          entity = await getCustomerByName(entityName);
+        }
+      } catch (error) {
+        console.error(`Error creating entity ${entityName}:`, error);
+      }
+    }
+    return entity;
+  };
+
   const insertShipmentIsf = async (data, transactionId, transactionType) => {
     const response = await soapStrategy(apiCredentials).createShipment(
       data,
@@ -18,84 +39,36 @@ module.exports = (apiCredentials, hyperion) => {
       transactionType
     );
     if (response.status === "Imported") {
-      const sh = await getShipmentByGuid(transactionId);
+      await getShipmentByGuid(transactionId);
 
-      let shipTo =
-        data.ShipTo &&
-        data.ShipTo[0] &&
-        (await getCustomerByName(data.ShipTo[0].Name));
-      if (!shipTo) {
-        const responseEntity =
-          data.ShipTo &&
-          data.ShipTo[0] &&
-          (await soapApiEntity(apiCredentials).createEntity(
-            data.ShipTo && data.ShipTo[0]
-          ));
-        if (responseEntity && responseEntity.status === "Imported") {
-          shipTo = await getCustomerByName(data.ShipTo[0].Name);
-        }
-      }
-      let consolidators =
-        data.Consolidators &&
-        data.Consolidators[0] &&
-        (await getCustomerByName(data.Consolidators[0].Name));
-      if (!consolidators) {
-        const responseEntity =
-          data.Consolidators &&
-          data.Consolidators[0] &&
-          (await soapApiEntity(apiCredentials).createEntity(
-            data.Consolidators && data.Consolidators[0]
-          ));
-        if (responseEntity && responseEntity.status === "Imported") {
-          consolidators = await getCustomerByName(data.Consolidators[0].Name);
-        }
-      }
-      let stuffingLocations =
-        data.StuffingLocations &&
-        data.StuffingLocations[0] &&
-        (await getCustomerByName(data.StuffingLocations[0].Name));
-      if (!stuffingLocations) {
-        const responseEntity =
-          data.StuffingLocations &&
-          data.StuffingLocations[0] &&
-          (await soapApiEntity(apiCredentials).createEntity(
-            data.StuffingLocations && data.StuffingLocations[0]
-          ));
-        if (responseEntity && responseEntity.status === "Imported") {
-          stuffingLocations = await getCustomerByName(
-            data.StuffingLocations[0].Name
-          );
-        }
-      }
-      let importer =
-        data.Importer && (await getCustomerByName(data.Importer.Name));
-      if (!importer) {
-        const responseEntity =
-          data.Importer &&
-          (await soapApiEntity(apiCredentials).createEntity(data.Importer));
-        if (responseEntity && responseEntity.status === "Imported") {
-          importer = await getCustomerByName(data.Importer.Name);
-        }
-      }
-      let bookingParty =
-        data.BookingParty &&
-        data.BookingParty[0] &&
-        (await getCustomerByName(data.BookingParty[0].Name));
-      if (!bookingParty) {
-        const responseEntity =
-          data.BookingParty &&
-          data.BookingParty[0] &&
-          (await soapApiEntity(apiCredentials).createEntity(
-            data.BookingParty && data.BookingParty[0]
-          ));
-        if (responseEntity && responseEntity.status === "Imported") {
-          bookingParty = await getCustomerByName(data.BookingParty[0].Name);
-        }
+      if (data.ShipTo && data.ShipTo[0]) {
+        await getOrCreateEntity(data.ShipTo[0], data.ShipTo[0].Name);
       }
 
-      console.log("Done!!");
-    } else {
-      console.log("Error!!");
+      if (data.Consolidators && data.Consolidators[0]) {
+        await getOrCreateEntity(
+          data.Consolidators[0],
+          data.Consolidators[0].Name
+        );
+      }
+
+      if (data.StuffingLocations && data.StuffingLocations[0]) {
+        await getOrCreateEntity(
+          data.StuffingLocations[0],
+          data.StuffingLocations[0].Name
+        );
+      }
+
+      if (data.Importer) {
+        await getOrCreateEntity(data.Importer, data.Importer.Name);
+      }
+
+      if (data.BookingParty && data.BookingParty[0]) {
+        await getOrCreateEntity(
+          data.BookingParty[0],
+          data.BookingParty[0].Name
+        );
+      }
     }
     return response;
   };
